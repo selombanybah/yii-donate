996be58fd03d0ff2a5dc0ee12ceefd6366db5c8c
mithereal@gmail.com <mithereal@gmail.com@17d9504b-fd66-3127-e4af-dad0333f97d3>
2010-12-27 08:35:32 +0000

    donate via paypal

---
+++ trunk/Donate.php
@@ -0,0 +1,39 @@
+﻿<?php
+/**
+ * Donate component.
+ * This implements a donation component for yii 
+ * 
+ * 2010 Jason Clark
+ *
+ */
+
+class Donate extends CWidget
+{
+	public $email;
+	public $item='A Good Cause';
+	public $currency;
+	public $amt='USD';
+	protected $form;
+        
+       
+    function init()
+     {
+	$this->form='
+	<div id="donate_paypal" class="donate">
+	<form name="_xclick" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+	<input type="hidden" name="cmd" value="_xclick">
+	<input type="hidden" name="business" value="'.$email.'">
+	<input type="hidden" name="item_name" value="'.$this->item.'">
+	<input type="hidden" name="currency_code" value="'.$this->currency.'">
+	<input type="hidden" name="amount" value="'.$this->amt.'">
+	<input type="image" src="http://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Make payments with PayPal - it\'s fast, free and secure!">
+	</form>
+	</div>';
+    }
+	
+    public function run()
+    {
+	echo $this->form;
+    }
+    
+}
