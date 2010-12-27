68a11d9ae831a7e81755eef020b349a7778a1845
mithereal@gmail.com <mithereal@gmail.com@17d9504b-fd66-3127-e4af-dad0333f97d3>
2010-12-27 09:31:18 +0000

    made some layout changes, assets need to be created and managed

--- Donate.php
+++ Donate.php
@@ -39,30 +39,36 @@ class Donate extends CWidget
 	    break;
 	    
 	    case 'alertpay':
-	    $this->form='form method="post" action="https://www.alertpay.com/PayProcess.aspx" target="_blank" class="payment_form alertpay" >
-				<input type="hidden" name="ap_purchasetype" value="item"/>
-				<input type="hidden" name="ap_merchant" value="'.$this->email.'"/>
-				<input type="hidden" name="ap_itemname" value="'. $this->item .'"/>
-				<input type="hidden" name="ap_itemcode" value="'. $ithis->code .'"/> 
-				<input type="hidden" name="ap_quantity" value="'.$this->quantity.'"/>
-				<input type="hidden" name="ap_returnurl" value=""/>
-				<input type="hidden" name="ap_currency" value="<?php print.' $this->currency.'; ?>"/>
-				<label>' $this->currency.' <input type="text" name="'.$this->amt.'" value="'.$this->amt.'" size="3" /></label><br/>
-				<input type="image" name="ap_image" src="<?php print $this->uri; ?>/images/alertpay_logo.png" width="90" height="60" alt="Donate with AlertPay" style="border: none; background: none;" />
-			</form>';
+		$this->form='
+		<div id="donate_alertpay" class="donate">
+		<form method="post" action="https://www.alertpay.com/PayProcess.aspx" target="_blank" class="payment_form alertpay" >
+		<input type="hidden" name="ap_purchasetype" value="item"/>
+		<input type="hidden" name="ap_merchant" value="'.$this->email.'"/>
+		<input type="hidden" name="ap_itemname" value="'. $this->item .'"/>
+		<input type="hidden" name="ap_itemcode" value="'. $ithis->code .'"/> 
+		<input type="hidden" name="ap_quantity" value="'.$this->quantity.'"/>
+		<input type="hidden" name="ap_returnurl" value=""/>
+		<input type="hidden" name="ap_currency" value="<?php print.' $this->currency.'; ?>"/>
+		<label>' $this->currency.' <input type="text" name="'.$this->amt.'" value="'.$this->amt.'" size="3" /></label><br/>
+		<input type="image" name="ap_image" src="<?php print $this->uri; ?>/images/alertpay_logo.png" width="90" height="60" alt="Donate with AlertPay" style="border: none; background: none;" />
+		</form>
+		</div>';
 	    break;
 	    
 	    case 'moneybookers':
-	    $this->form='<form action="https://www.moneybookers.com/app/payment.pl" method="post" target="_blank" class="payment_form moneybookers" >
-				<input type="hidden" name="pay_to_email" value="'. $this->email.'" />
-				<input type="hidden" name="language" value="'.$this->language.'" />
-				<input type="hidden" name="rid" value="5413099" />
-				<label >'.$this->currency.' <input type="text" name="amount" value="'. $this->amt.'" size="3" /></label><br/>
-				<input type="hidden" name="currency" value="'. $this->currency .'" />
-				<input type="hidden" name="detail1_description" value="'.$this->code .'" />
-				<input type="hidden" name="detail1_text" value="'.$this->item .'" />
-				<input type="image" src="<?php print $this->uri; ?>/images/mb_orange_donate_with.gif" width="90" height="60" border="0" name="submit" alt="Donate with Moneybookers" style="border: none; background: none;" />
-			</form>';
+		$this->form='
+		<div id="donate_moneybookers" class="donate">
+		<form action="https://www.moneybookers.com/app/payment.pl" method="post" target="_blank" class="payment_form moneybookers" >
+		<input type="hidden" name="pay_to_email" value="'. $this->email.'" />
+		<input type="hidden" name="language" value="'.$this->language.'" />
+		<input type="hidden" name="rid" value="5413099" />
+		<label >'.$this->currency.' <input type="text" name="amount" value="'. $this->amt.'" size="3" /></label><br/>
+		<input type="hidden" name="currency" value="'. $this->currency .'" />
+		<input type="hidden" name="detail1_description" value="'.$this->code .'" />
+		<input type="hidden" name="detail1_text" value="'.$this->item .'" />
+		<input type="image" src="<?php print $this->uri; ?>/images/mb_orange_donate_with.gif" width="90" height="60" border="0" name="submit" alt="Donate with Moneybookers" style="border: none; background: none;" />
+		</form>
+		</div>';
 	    break;
 	}
     }
